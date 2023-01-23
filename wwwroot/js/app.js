cf3ebef13a7031023f40753587ce6b2684805618
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-23 15:40:16 +0100

    added text to About page and created Razor page

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -2,9 +2,11 @@
 		// Hamburger BTN 
         const toggleBtn = document.querySelector('.toggleBtn');
 		const sidebar = document.querySelector('.sidebar');
-
+		// add eventlistener for click
 		toggleBtn.addEventListener('click', () => {
+			//toggle active to add styling to class
 			toggleBtn.classList.toggle('active');
 			sidebar.classList.toggle('active');
 		});
         
+	
